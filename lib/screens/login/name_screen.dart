b99deb1756584de377938c7f85e8718e48634953
MinthangML <thangpi4216@gmail.com>
2025-10-20 23:58:40 +0630

    finished lesson 3

---
+++ lib/screens/login/name_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fitness/common/color_extension.dart';
+import 'package:flutter_fitness/common_widgets/round_button.dart';
+import 'package:flutter_fitness/common_widgets/round_text_field.dart';
+import 'package:flutter_fitness/screen/login/goal_screen.dart';
+
+class NameScreen extends StatefulWidget {
+  const NameScreen({super.key});
+
+  @override
+  State<NameScreen> createState() => _NameScreenState();
+}
+
+class _NameScreenState extends State<NameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 30),
+              Text(
+                "Enter Your Name",
+                style: TextStyle(
+                  color: TColor.primaryText,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const RoundTextField(hintText: "i.e code for any"),
+              const SizedBox(height: 40),
+              RoundButton(
+                title: "NEXT",
+                isPadding: false,
+                onPressed: () {
+                  context.push(const GoalScreen());
+                },
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
